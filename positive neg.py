ce7cbee76fc985defcc58bdf7ce68a0504989a82
mukundn393 <51233148+mukundn393@users.noreply.github.com>
2019-07-12 15:46:32 +0530

    Update positive neg.py

--- positive neg.py
+++ positive neg.py
@@ -1,7 +1,8 @@
-num = float(input())
-if num > 0:
-   print("Positive")
-elif num == 0:
-   print("Zero")
+n = float(input())
+if n >= 0:
+   if n == 0:
+       print("Zero")
+   else:
+       print("Positive")
 else:
    print("Negative")
