59aace492cea1271e775a2e528d1d3733cc85273
mukundn393 <51233148+mukundn393@users.noreply.github.com>
2019-07-12 16:12:39 +0530

    Create vowcon.py

---
+++ vowcon.py
@@ -0,0 +1,9 @@
+n=input()
+if((n>='a' and n<='z') or (n>='A' and n<='Z')):
+  n=n.lower()
+  if(n=='a' or n=='e' or n=='i' or n=='o' or n=='u'):
+     print("Vowel")
+  else:
+     print("Consonant")
+else:
+    print("Special Characters")
