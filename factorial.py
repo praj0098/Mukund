0d3482096941eb326211432094cb75101ff3b041
Prajwal Srivastava <53837956+praj0098@users.noreply.github.com>
2020-10-01 10:09:43 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -3,3 +3,11 @@ factori = 1
 for i in range(1,vijay+1):
   factori = factori*i
 print(factori)
+
+It can be done using recursiom
+
+def fact(num):
+  if num ==0 or num == 1:
+    return 1
+  else:
+    return num*fact(num-1)
